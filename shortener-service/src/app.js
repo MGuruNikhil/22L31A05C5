e780343228e0680d849e35613c5375746e027ba2
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2025-09-04 12:55:52 +0530

    cors

--- shortener-service/src/app.js
+++ shortener-service/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const shorturlsRouter = require('./routes/shorturls');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // mount API routes
 app.use('/', shorturlsRouter);
